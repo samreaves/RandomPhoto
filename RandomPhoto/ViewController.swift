600c083de0cdd41bd663795fc85cef92c04e91f2
Sam Reaves <thesamreaves@gmail.com>
2021-09-01 20:58:57 -0400

    Initial Commit

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by Samuel Reaves on 9/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
